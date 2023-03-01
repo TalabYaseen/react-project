a5848f28c5d533f85665a5658d3310027db6c4d6
unknown <AbrarMAlhillawi@gmail.com>
2023-03-02 00:09:26 +0300

    style

--- frontend/social_media/src/components/InfoPost.js
+++ frontend/social_media/src/components/InfoPost.js
@@ -3,14 +3,10 @@ import Like from './Like'
 import { AiFillEdit } from 'react-icons/ai';
 function InfoPost(props) {
   console.log(props.data,"props.data.profile_pic")
-  // this func make edite post appear
   const editPost = (id,content) => {
-    // console.log("editepost");
-    // document.getElementById(`post${props.data.post_id}`).style.display = 'none';
-    // document.getElementById(`editPostForm${props.data.post_id}`).style.display = 'block';
-    // document.getElementById(`editPostBTN${props.data.post_id}`).style.display = 'none';
     props.choosePostToEdit(id,content);
   }
+  const deletePost = () => {}
   return (
 
     <div>
@@ -21,11 +17,16 @@ function InfoPost(props) {
         <div className="friend-name">
           <ins><a href="time-line.html" title>{props.data.first_name} {props.data.last_name}</a></ins>
           <span>published: {props.data.created_at}</span>
-          {/* delete and edit post */}
+
+          {/* delete post */}
+          
           {(props.data.id == JSON.parse(localStorage.getItem("user")).id) ?
             <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-              <div> <button onClick={() => { deletePost(props.data.post_id) }}><i class="fa fa-trash" aria-hidden="true" style={{ color: "black", fontSize: '20px', marginLeft: '10px' }} /></button></div>
-              <div>     <button id={`editPostBTN${props.data.post_id}`} onClick={() => { editPost(props.data.post_id) }}> <AiFillEdit style={{ color: "black", fontSize: '20px' }} />
+              <div> <button onClick={() => { deletePost(props.data.post_id) }} style={{background:'none',border:'none'}}><i class="fa fa-trash" aria-hidden="true" style={{color:'black', fontSize: '30px' }} /></button>
+              </div>
+
+              {/* edit post */}
+              <div>     <button id={`editPostBTN${props.data.post_id}`} onClick={() => { editPost(props.data.post_id) }}  style={{marginLeft: '10px',background:'none',border:'none' }}> <AiFillEdit style={{ color: "black", fontSize: '30px'}} />
              
               </button>
 
